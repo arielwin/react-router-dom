45f78208dcb08efe7fd68793c9e097283a42b6dd
arielwin <subiboy7@gmail.com>
2024-08-01 09:00:00 -0600

    complete exercises

---
+++ src/components/PokemonDetails.jsx
@@ -0,0 +1,23 @@
+// src/components/PokemonDetails.jsx
+
+import { useParams } from 'react-router-dom'
+
+const PokemonDetails = (props) => {
+    const { pokemonId } = useParams()
+    const singlePokemon = props.pokemon.find(pokemon => pokemon._id === Number(pokemonId))
+
+    return (
+      <>
+        <h2>Pokemon Details: {singlePokemon.name}</h2>
+        <dl>
+          <dt>Weight:</dt>
+          <dd>{singlePokemon.weight}</dd>
+          <dt>Height:</dt>
+          <dd>{singlePokemon.height}</dd>
+        </dl>
+      </>
+    );
+  };
+  
+  export default PokemonDetails;
+  
